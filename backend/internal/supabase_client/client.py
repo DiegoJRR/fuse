6abe2afd50aa3fa46adc06b0002b07a8c155e2c8
Efrainq07 <efrainq07@hotmail.com>
2024-10-19 22:34:27 -0700

    added supabase client

---
+++ backend/internal/supabase_client/client.py
@@ -0,0 +1,8 @@
+import os
+from supabase import create_client, Client
+
+
+SUPABASE_URL : str = os.environ.get("SUPABASE_URL")
+SUPABASE_KEY : str = os.environ.get("SUPABASE_KEY")
+
+supabase_client: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
