72983823eb1fbafdf496b1d601020459eba3e1b1
Efrainq07 <efrainq07@hotmail.com>
2024-10-20 08:12:04 -0700

    xd

--- backend/internal/signer.py
+++ backend/internal/signer.py
@@ -1,21 +1,21 @@
-import os
-from dotenv import load_dotenv
-load_dotenv()
+#import os
+#from dotenv import load_dotenv
+#load_dotenv()
 
-import hashlib
-from web3 import Web3, EthereumTesterProvider, eth
-from eth_account.messages import encode_defunct
+#import hashlib
+#from web3 import Web3, EthereumTesterProvider, eth
+#from eth_account.messages import encode_defunct
 
-WALLET_PRIVATE_KEY = os.getenv('WALLET_PRIVATE_KEY')
-w3 = Web3(Web3.HTTPProvider('https://sepolia.infura.io/v3/'))
+#WALLET_PRIVATE_KEY = os.getenv('WALLET_PRIVATE_KEY')
+#w3 = Web3(Web3.HTTPProvider('https://sepolia.infura.io/v3/'))
 
-account = w3.eth.account.from_key(WALLET_PRIVATE_KEY)
-def signContract(to,collectible_type_ids,amounts, contract_address):
+#account = w3.eth.account.from_key(WALLET_PRIVATE_KEY)
+#def signContract(to,collectible_type_ids,amounts, contract_address):
     # to = '0x3980dDB72A01ECF127c79C2b08865B9583471321'  # Destination address
     # collectible_type_ids = [1]  # Assuming these are uint256 values
     # amounts = [1]  # Assuming these are uint256 values
     # contract_address = '0x7E7b8cdD92766a19c2dAbB7CD6750901206d7c6e'  # Contract address
 
-    encoded_data = Web3.solidity_keccak(["address", "string[]", "uint256[]", "address"], [to, collectible_type_ids, amounts, contract_address])
-    signed_message = account.sign_message(encode_defunct(encoded_data))
-    return(signed_message)  
+#    encoded_data = Web3.solidity_keccak(["address", "string[]", "uint256[]", "address"], [to, collectible_type_ids, amounts, contract_address])
+ #   signed_message = account.sign_message(encode_defunct(encoded_data))
+ #   return(signed_message)  
