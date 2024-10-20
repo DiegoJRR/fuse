1125aa6b932dcf951fb9872dd3c9a05aa1762d1f
Diego de Jesús Ramírez Rodríguez <diegojrr00@gmail.com>
2024-10-20 03:36:00 -0700

    add cors policy to api

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from typing import Union
 import json
 from pydantic import BaseModel
@@ -14,6 +15,21 @@ dspy.settings.configure(lm=lm)
 from .internal import walrus, concepts, db, ai_gen
 
 app = FastAPI()
+
+# Define the allowed origins
+origins = [
+    "http://localhost:3000",  # Example of a front-end app origin
+    "https://fuse-gzf35clch-efrain-quinteros-projects.vercel.app/",     # You can also specify your production domain
+]
+
+# Add the CORSMiddleware to the application
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # List of allowed origins
+    allow_credentials=True,  # Allow cookies to be sent with cross-origin requests
+    allow_methods=["*"],     # Allow all HTTP methods (GET, POST, PUT, DELETE, etc.)
+    allow_headers=["*"],     # Allow all headers
+)
 
 
 PUBLISHER = "https://walrus-testnet-publisher.nodes.guru"
