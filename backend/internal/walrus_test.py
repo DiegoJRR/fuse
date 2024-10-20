b1787c3c7a6552c9ee404351b2bd6b009599ba2b
Efrainq07 <efrainq07@hotmail.com>
2024-10-19 22:38:18 -0700

    updated walrus test for new walrus class structure

--- backend/internal/walrus_test.py
+++ backend/internal/walrus_test.py
@@ -6,18 +6,13 @@ aggregator = "https://aggregator.walrus-testnet.walrus.space"
 walrus_client = walrus.Walrus(publisher, aggregator)
 
 # Store a file
-with open("apple_icon.jpg", "rb") as file:
+with open("fuse.jpg", "rb") as file:
     file_data = file.read()
 
-response = walrus_client.store_blob(file_data, epochs=5)
-print("Stored file response:", response)
+blob_id = walrus_client.store_blob(file_data, epochs=5)
+print(blob_id)
+print("Stored file response:", blob_id)
 
-# Retrieve a blob
-blob_id = ""
-if "newlyCreated" in response.keys():
-    blob_id = response['newlyCreated']['blobObject']['blobId']
-else:
-    blob_id = response['alreadyCertified']['blobId']
 
 # Retrieve a blob and save to file
 blob_bytes = walrus_client.retrieve_blob(blob_id)
