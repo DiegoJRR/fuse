02fe89d50fc0bd152d9e4039c6737ecc63e65af3
Diego de Jesús Ramírez Rodríguez <diegojrr00@gmail.com>
2024-10-20 06:14:19 -0700

    change contract

--- backend/internal/signer.py
+++ backend/internal/signer.py
@@ -14,7 +14,7 @@ account = w3.eth.account.from_key(WALLET_PRIVATE_KEY)
 to = '0x3980dDB72A01ECF127c79C2b08865B9583471321'  # Destination address
 collectible_type_ids = [1]  # Assuming these are uint256 values
 amounts = [1]  # Assuming these are uint256 values
-contract_address = '0x5e1DD38E704C924353f5bfCE2c5C69439404A098'  # Contract address
+contract_address = '0x7E7b8cdD92766a19c2dAbB7CD6750901206d7c6e'  # Contract address
 
 encoded_data = Web3.solidity_keccak(["address", "uint256[]", "uint256[]", "address"], [to, collectible_type_ids, amounts, contract_address])
 signed_message = account.sign_message(encode_defunct(encoded_data))
