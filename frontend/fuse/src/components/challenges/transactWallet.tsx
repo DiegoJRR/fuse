f1318f2a29b49658c8d88e3d453f8c06d8ce6aef
nostalgic-math-student <josue.rojas@ciencias.unam.mx>
2024-10-20 08:58:03 -0700

    si

--- frontend/fuse/src/components/challenges/transactWallet.tsx
+++ frontend/fuse/src/components/challenges/transactWallet.tsx
@@ -7,25 +7,18 @@ import {ethers} from 'ethers'
 import { sign } from 'crypto';
 import {fetchUriData} from '../../app/fetchUris';
 
-const getCookie = (name: string): string | null => {
-  const match = document.cookie.match(new RegExp("(^| )" + name + "=([^;]+)"));
-  if (match && match[2]) {
-    return decodeURIComponent(match[2]);
-  }
-  return null;
-};
 
 export default function MintNFT() {
   
   
-  const session_id = getCookie("sessionId");
+  const session_id = "9aa471b2-caa1-4f5d-acf0-ab0c2a64a729";
   
   const { primaryWallet } = useDynamicContext();
 
     const session_URIs = async () => {
       try {
         // Fetch the session URIs
-        const session_URIs = await fetchUriData(session_id||"");
+        const session_URIsAnswer = await fetchUriData(session_id||"");
 
       } catch (error) {
         console.error("Error setting session cookie:", error);
@@ -33,35 +26,43 @@ export default function MintNFT() {
     };
     // const to = '0x3749367e53B6fdf5ceDb2D6BCEf7d740C7074885';
     const collectibleURIs = session_URIs.uris; // Example URIs
-    const amounts = [BigInt(session_URIs.uris.length)]; // Example amounts
-    const to = primaryWallet?.address; // Replace with the recipient address
+    const amounts = [BigInt(session_URIs.uris||0)]; // Example amounts
+    const to = primaryWallet?.address || ""; // Replace with the recipient address
+
+    // const signature = '0x1bf25efeff5e4b25d3196bf3ecbfe76e03b1a7fddc8f5a31e837ce14122c3326171b225bb1d276495c93957cd31d236743395a0275eb76a9e5fe3cf3be94ef211c'; // Example signature
 
     
-    const signature = '0x1bf25efeff5e4b25d3196bf3ecbfe76e03b1a7fddc8f5a31e837ce14122c3326171b225bb1d276495c93957cd31d236743395a0275eb76a9e5fe3cf3be94ef211c'; // Example signature
-
   // Proper setup of useWriteContract
  
 
   const handleTransfer = async () => {
+
+    const network = primaryWallet?.getNetwork()
+
+    const contracts = {
+      sepolia:"0x31c56d02562267637f62fb3f0e84630cd73ab7d7",
+      flowTest:"0x93dD4f56332fa1B441422a55f4B958f75abFfB46",
+      polygonMainnet:"0xe72c8E6304B65A2971f1Ea7E49839E07dff93851"
+
+    }
+
     if (!primaryWallet) return;
     const provider = await getWeb3Provider(primaryWallet)
     const signer = await getSigner(primaryWallet)
     const contractInstance = new ethers.Contract(
-        '0xe38b618cee9e26221da3c2ba52405580355987fc',
+        '0x31c56d02562267637f62fb3f0e84630cd73ab7d7',
         abi,
-        signer
     )
-    let sig_encode = ethers.hexlify(signature)
-    console.log(sig_encode,"encode")
-    let result = await contractInstance.mintBatchWithURIsAndSignature!(to, collectibleURIs, amounts, signature)
 
-    console.log(result)
+    console.log(to,collectibleURIs,amounts)
+    
+    let result = await contractInstance.mintBatchWithURIs!(to, collectibleURIs, amounts)
 
   };
 
   return (
-    <div>
-      <button onClick={handleTransfer}>Transfer</button>
+    <div className="btn btn-active bg-green-500 text-white font-bold py-2 px-4 rounded-full">
+      <button onClick={handleTransfer}>Mint your concepts</button>
     </div>
   );
 }
