cebf96f279ba5f298746c90fb0298d2a7717a832
Efrainq07 <efrainq07@hotmail.com>
2024-10-19 22:38:58 -0700

    added new function to concepts utilities

--- backend/internal/concepts.py
+++ backend/internal/concepts.py
@@ -1,16 +1,17 @@
 import re
 
-def get_concepts_key(first_concept: str, second_concept: str) -> str:
+def format_order_concepts(first_concept: str, second_concept: str) -> str:
     concepts = [first_concept.lower(), second_concept.lower()]
-
-    # Remove special characters, allowing only ASCII
     cleaned_concepts = [re.sub(r'[^a-z0-9]', '', concept) for concept in concepts]
 
-    if cleaned_concepts[0] == "" or cleaned_concepts[1] == "":
-        return ""
-
     cleaned_concepts.sort()
+    
+    return cleaned_concepts
 
-    # Combine with an underscore
-    combined_result = '_'.join(cleaned_concepts)
+def get_concepts_key(first_concept: str, second_concept: str) -> str:
+    
+    concepts = format_order_concepts(first_concept, second_concept)
+
+    combined_result = '_'.join(concepts)
+    
     return combined_result
